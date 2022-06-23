2153b5739d4fedf09358bb998a7f42e0ddf72006
addumairi <azharaddumairi@gmail.com>
2022-06-23 10:14:54 +0800

    Sprint-1 Add fitur User Login,Logout,Forget Password

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+// import { createStore } from "redux";
 import { Reducers } from "./reducers";
+import { BrowserRouter } from 'react-router-dom';
+// configure redux
+import userReducer from "./redux/reducers/user-reducer";
+import { createStore,combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from "redux-thunk";
+
 
 const storeReducer = createStore(Reducers);
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={storeReducer}>
+  <BrowserRouter>
+   <Provider store={storeReducer}  >
     <App />
   </Provider>
+  </BrowserRouter>
+ 
 );
